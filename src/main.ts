2ec78dca8c6e573264b0abd39a92127a09846268
afk-sistems <ti.leidyinmob@gmail.com>
2025-08-28 22:04:54 -0500

    Fix UI and laboral information

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ const tecnologies:string[] = ['Angular', 'NestJS', 'MySQL', 'TypeScript', 'Tailw
 
 const projects:IProject[] = [
   {title: 'Leidy Inmobiliaria', description: 'Página web para que los clientes puedan conocer a la empresa, sus beneficios, productos brindados y sus respectivos contactos', tecnologies: ['Angular', 'Firebase', 'TailwindCSS'], githubLink: null, liveLink: 'https://leidyinmobiliaria.com'},
+  {title: 'XML Sunat', description: 'Paquete de Node para la firma de comprobantes electrónicos (XML) para la SUNAT', tecnologies: ['NodeJS', 'TypeScript'], githubLink: 'https://github.com/jim-ux16/supernova_team_xml_sunat.git', liveLink: 'https://www.npmjs.com/package/@supernova-team/xml-sunat'}
 ];
 
 const jobs:IJob[] = [
+  {title: 'Desarolllador Web', company: 'Leidy Inmobiliaria', startMonthYear: 'Jul. 2024', endMonthYear: 'Ago. 2025'},
   {title: 'Practicante Desarrollo', company: 'Sunarp', startMonthYear: 'Set. 2023', endMonthYear: 'Jun. 2024'},
-  {title: 'Desarolllador Web', company: 'Leidy Inmobiliaria', startMonthYear: 'Jun. 2022', endMonthYear: 'Set. 2023'},
   {title: 'Practicante Desarrollo', company: 'Ortopedia Vicet', startMonthYear: 'Oct. 2021', endMonthYear: 'Ene. 2022'},
 ];
 
@@ -45,10 +46,10 @@ projects.forEach((project) => {
 
       <header class="flex gap-3 items-center">
         <i class="pi pi-code text-violet-500 text-2xl"></i>
-        <h3 class="text-white font-semibold text-xl">${project.title}</h3>
+        <h3 class="text-white font-semibold sm:text-xl text-lg">${project.title}</h3>
       </header>
       <p class="text-gray-400 mt-5 font-light">${project.description}</p>
-      <ul class="flex gap-3 mt-5"> 
+      <ul class="flex flex-wrap gap-3 mt-5"> 
         ${
           project.tecnologies.map((tec:string) => {
             return `<li class="rounded-lg bg-gray-900 p-2 border border-gray-600 text-gray-200 shadow">${tec}</li>`
